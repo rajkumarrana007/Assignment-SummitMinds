6762430d9fa6247cb8817dff26e1b11fdc27dfbb
rajkumarrana007 <58626222+rajkumarrana007@users.noreply.github.com>
2021-06-07 08:13:49 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,14 @@
+#Assignment: Using Python
+
+import json
+
+data = json.load(open("prize.json"))
+#print(data)
+
+for i, j in data.items();
+	for index, data in enumerate(j);
+	data = data
+	year = input("Enter Year :")
+	if data["year"] == year;
+	print(data)
+	continue
